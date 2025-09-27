1243a547855461c911a2c8537999070e27665cb2
Mekariyas <mekariyas05@gmail.com>
2025-09-27 19:15:25 +0300

    fixed ErrorPage UI

--- frontend/src/ErrorPage.tsx
+++ frontend/src/ErrorPage.tsx
@@ -3,10 +3,10 @@ import{ useNavigate } from "react-router-dom"
 const ErrorPage = ()=>{
     const navigate = useNavigate()
     return(
-    <section className="w-full h-[100vh] flex justify-center items-center">
-        <h1 className="font-bold w-full text-2xl">OOPS...</h1>
+    <section className="w-full h-[100vh] flex flex-col justify-center items-center gap-4">
+        <h1 className="font-bold w-full text-2xl text-center">OOPS...</h1>
         <p className="italic semi-bold text-lg mt-2">Page not found</p>
-        <button className="w-24 h-16 text-lg font-bold bg-red-600 text-white mt-3" onClick={()=>navigate(-1)}>Go back</button>
+        <button className="w-[35%] md:w-[25%] h-16 text-lg font-bold bg-red-600 text-white mt-3 rounded-md cursor-pointer" onClick={()=>navigate(-1)}>Go back</button>
     </section>)
 }
 
