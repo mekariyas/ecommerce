55e3fbe8486aea054dd5cfb27a49499a2c232317
Mekariyas <mekariyas05@gmail.com>
2026-05-13 19:19:01 +0300

    fixed type error on Orders page

--- backend/controllers/admin.js
+++ backend/controllers/admin.js
@@ -1,243 +1,374 @@
-import bcrypt from "bcryptjs"
-import jwt from "jsonwebtoken"
-import User from "../models/user.js"
-import Product from "../models/product.js"
-import Order from "../models/order.js"
-
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import User from "../models/user.js";
+import Product from "../models/product.js";
+import Order from "../models/order.js";
 
 //log in and log out(get request)
 //add a new product (post request)
 //restock a product(put request)
 //delete a product(delete request)
 
-
-
 //admin
 
-const refresh =(req, res)=>{
-    const refreshToken = req.cookies.userCookie;
-    if (!refreshToken){
-        return res.status(401).json({ message: "No token" })
-    };
-    
+const refresh = (req, res) => {
+  const refreshToken = req.cookies.userCookie;
+  if (!refreshToken) {
+    return res.status(401).json({ message: "No token" });
+  }
+
   try {
     const decoded = jwt.verify(refreshToken, process.env.ADMIN_SECRET_TOKEN);
     const accessToken = jwt.sign(
       { _id: decoded._id, role: decoded.role },
       process.env.ADMIN_SECRET_TOKEN,
-      { expiresIn: "15m" }
+      { expiresIn: "15m" },
     );
     res.json({ accessToken });
   } catch (err) {
     res.status(403).json({ message: "Invalid refresh token" });
   }
-}
+};
 
-const login  = async (req,res)=>{
-    const { email, password } = req.body
-    try{
-        const getUser  = await User.findOne({email:email})
-        if(!getUser){
-            return res.status(404).json({message: "User Does not Exist", success: false})
-        }
-        if(getUser.role !=="admin"){
-            return res.status(403).json({message: "Access denied: Admins only", success: false})
-        }
-        const verifyUser  = await bcrypt.compare(password, getUser.password)
-        if(!verifyUser){
-            return res.status(401).json({message: "Incorrect password", success: false})
-        }
-        const accessToken = jwt.sign({_id: getUser._id, role: getUser.role},process.env.ADMIN_SECRET_TOKEN,{expiresIn: "15m"})
-        const refreshToken = jwt.sign({_id: getUser._id, role: getUser.role}, process.env.ADMIN_SECRET_TOKEN,{expiresIn: "168h"})
-
-        res.cookie("adminCookie", refreshToken, {
-            maxAge : 7 * 24 * 60 * 60 * 1000,
-            httpOnly: true,
-            sameSite: "lax",
-            secure: process.env.NODE_ENV === "production"
-        })
-        return res.status(200).json({message:"successfully authorized", success: true, accessToken, id:getUser.id})
-    
-    }catch(error){
-        console.log(error.message)
-        return res.status(500).json({message:"Internal Server Error", success: false})
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const getUser = await User.findOne({ email: email });
+    if (!getUser) {
+      return res
+        .status(404)
+        .json({ message: "User Does not Exist", success: false });
     }
-}
-
-
-
-const logout = async(req, res)=>{
-    const cookie = req.cookies?.adminCookie
-
-    if (!cookie){
-        return res.status(404).json({message: "cookie not found", success: false})
+    if (getUser.role !== "admin") {
+      return res
+        .status(403)
+        .json({ message: "Access denied: Admins only", success: false });
     }
+    const verifyUser = await bcrypt.compare(password, getUser.password);
+    if (!verifyUser) {
+      return res
+        .status(401)
+        .json({ message: "Incorrect password", success: false });
+    }
+    const accessToken = jwt.sign(
+      { _id: getUser._id, role: getUser.role },
+      process.env.ADMIN_SECRET_TOKEN,
+      { expiresIn: "15m" },
+    );
+    const refreshToken = jwt.sign(
+      { _id: getUser._id, role: getUser.role },
+      process.env.ADMIN_SECRET_TOKEN,
+      { expiresIn: "168h" },
+    );
 
-    res.clearCookie("adminCookie",{
+    res.cookie("adminCookie", refreshToken, {
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+      httpOnly: true,
+      sameSite: "lax",
+      secure: process.env.NODE_ENV === "production",
+    });
+    return res
+      .status(200)
+      .json({
+        message: "successfully authorized",
+        success: true,
+        accessToken,
+        id: getUser.id,
+      });
+  } catch (error) {
+    console.log(error.message);
+    return res
+      .status(500)
+      .json({ message: "Internal Server Error", success: false });
+  }
+};
+
+const logout = async (req, res) => {
+  const cookie = req.cookies?.adminCookie;
+
+  if (!cookie) {
+    return res
+      .status(404)
+      .json({ message: "cookie not found", success: false });
+  }
+
+  res.clearCookie("adminCookie", {
     httpOnly: true,
     sameSite: "lax",
-    secure: process.env.NODE_ENV === "production"})
-    
-    return res.status(200).json({message: "successfully logged out", success: true})
-}
+    secure: process.env.NODE_ENV === "production",
+  });
 
+  return res
+    .status(200)
+    .json({ message: "successfully logged out", success: true });
+};
 
-const getAdmin = async(req, res) => {
-    try{
-        const { id } = req.params
-        const admin = await User.findById(id)
-        if(!admin){
-            return res.status(404).json({message: "Admin not found"})
-        }
-        const accessToken = jwt.sign({_id: admin._id, role:admin.role},process.env.SECRET_TOKEN,{expiresIn: "15m"})
-        return res.status(200).json({firstName: admin.fName ,lastName: admin.lName, accessToken})
+const getAdmin = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const admin = await User.findById(id);
+    if (!admin) {
+      return res.status(404).json({ message: "Admin not found" });
     }
-    catch(error){ 
-        console.log("Error Admin")  
-        console.log(error.message)
-        return res.status(500).json({message:"Internal Server Error", success: false})
-    }
-}
-
+    const accessToken = jwt.sign(
+      { _id: admin._id, role: admin.role },
+      process.env.SECRET_TOKEN,
+      { expiresIn: "15m" },
+    );
+    return res
+      .status(200)
+      .json({ firstName: admin.fName, lastName: admin.lName, accessToken });
+  } catch (error) {
+    console.log("Error Admin");
+    console.log(error.message);
+    return res
+      .status(500)
+      .json({ message: "Internal Server Error", success: false });
+  }
+};
 
 //Product CRUD
 
-const addProduct = async(req, res)=>{
-    const { name, price, brand, description, size ,stock, color} = req.body
-    const image = req.file?.filename
+const addProduct = async (req, res) => {
+  const { name, price, brand, description, size, stock, color } = req.body;
+  const image = req.file?.filename;
+  const adminId = req.userId;
+  if (
+    !name ||
+    !price ||
+    !brand ||
+    !description ||
+    !size ||
+    !stock ||
+    !color ||
+    !image
+  ) {
+    return res.status(400).json({ message: "Incomplete data", success: false });
+  }
+
+  try {
+    const itemStored = await Product.findOne({ name: name });
+    if (itemStored) {
+      return res
+        .status(409)
+        .json({ message: "Item already exists", success: false });
+    }
+    await Product.create({
+      name: name,
+      price: Number(price),
+      brand: brand,
+      description: description,
+      stock: Number(stock),
+      size: size.split(","),
+      color: color.split(","),
+      image: image,
+    });
+
+    const accessToken = jwt.sign(
+      { _id: decodedToken._id, role: decodedToken.role },
+      process.env.SECRET_TOKEN,
+      { expiresIn: "15m" },
+    );
+    return res.status(201).json({ message: "Item Created", accessToken });
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).json({ message: "Internal server Error" });
+  }
+};
+
+const getProducts = async (req, res) => {
+  try {
+    const { skip } = req.queryData;
+    const totalProducts = await Product.countDocuments({});
+    const products = await Product.find({ stock: { $gt: 0 } })
+      .skip(skip)
+      .limit(5);
     const adminId = req.userId;
-    if( !name || !price || !brand || !description || !size  ||!stock ||!color || !image ){
-        return res.status(400).json({message: "Incomplete data", success: false})
-    }
+    const accessToken = jwt.sign(
+      { _id: adminId, role: "admin" },
+      process.env.SECRET_TOKEN,
+      { expiresIn: "15m" },
+    );
 
-    try{
-        const itemStored = await Product.findOne({name: name})
-        if(itemStored){
-            return res.status(409).json({message:"Item already exists", success:false})
-        }
-        await Product.create({name:name, price:Number(price), brand:brand, description: description, stock: Number(stock), size: size.split(","), color: color.split(","), image:image})
-        
-        const accessToken = jwt.sign({_id: decodedToken._id, role:decodedToken.role},process.env.SECRET_TOKEN,{expiresIn: "15m"})
-        return res.status(201).json({message:"Item Created", accessToken})
-    }catch(error){
-        console.log(error.message)
-        return res.status(500).json({message:"Internal server Error"})    
-    }
-}
+    return res
+      .status(200)
+      .json({ products: products, totalProducts, accessToken });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Internal server error", success: false });
+  }
+};
 
-const getProducts = async(req, res)=>{
-    try{
-        const { skip } =  req.queryData
-        const totalProducts = await Product.countDocuments({});
-        const products = await  Product.find({stock:{$gt:0}}).skip(skip).limit(5);
-        const adminId = req.userId;
-        const accessToken = jwt.sign({_id: adminId, role:"admin"},process.env.SECRET_TOKEN,{expiresIn: "15m"})
-        
-        return res.status(200).json({products:products, totalProducts, accessToken})
-    }catch(error){
-        return res.status(500).json({message: "Internal server error", success: false})
+const getProduct = async (req, res) => {
+  const { name } = req.params;
+  const adminId = req.userId;
+  if (!name) {
+    return res
+      .status(400)
+      .json({ message: "No query defined", success: false });
+  }
+  try {
+    const item = await Product.findOne({ name: name });
+    if (!item) {
+      return res
+        .status(404)
+        .json({ message: "Item not found", success: false });
     }
-}
+    const accessToken = jwt.sign(
+      { _id: adminId, role: "admin" },
+      process.env.SECRET_TOKEN,
+      { expiresIn: "15m" },
+    );
+    return res.status(200).json({ item, accessToken });
+  } catch (error) {
+    console.log(error.message);
+    return res
+      .status(500)
+      .json({ message: "Internal Server Error", success: false });
+  }
+};
 
-const getProduct  = async(req, res)=>{
-    const { name } = req.params;
+const restockProduct = async (req, res) => {
+  const { name, price, brand, description, stock, color } = req.body;
+  const image = req.file?.filename ?? req.body.image;
+  const adminId = req.userId;
+  if (!name) {
+    return res
+      .status(401)
+      .json({ message: "Product name not provided", success: false });
+  }
+  try {
+    await Product.findOneAndUpdate(
+      { name: name },
+      {
+        name: name,
+        price: price,
+        brand: brand,
+        description: description,
+        stock: stock,
+        color: color,
+        image: "../uploads/shoe-items/" + image,
+      },
+    );
+    const accessToken = jwt.sign(
+      { _id: adminId, role: "admin" },
+      process.env.SECRET_TOKEN,
+      { expiresIn: "15m" },
+    );
+    return res
+      .status(200)
+      .json({ message: "Product Updated", success: true, accessToken });
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+const deleteProduct = async (req, res) => {
+  const { name } = req.body;
+  const adminId = req.userId;
+  if (!name) {
+    return res
+      .status(401)
+      .json({ message: "Product name not provided", success: false });
+  }
+  try {
+    const findItem = await Product.findOne({ name: name });
+    const accessToken = jwt.sign(
+      { _id: adminId, role: "admin" },
+      process.env.SECRET_TOKEN,
+      { expiresIn: "15m" },
+    );
+    if (!findItem) {
+      return res
+        .status(404)
+        .json({ message: "Product does not exist", success: false });
+    }
+    await Product.findOneAndDelete({ name: name });
+    return res
+      .status(204)
+      .json({ message: "Product deleted", success: true, accessToken });
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+const getOrders = async (req, res) => {
+  try {
     const adminId = req.userId;
-    if(!name){
-        return res.status(400).json({message: "No query defined", success:false})
+    const skip = parseInt(req.query.skip) || 0;
+    const totalOrders = await Order.countDocuments({});
+    const orders = await Order.find({ status: "Pending" })
+      .skip(skip)
+      .limit(5)
+      .sort({ createdAt: -1 });
+    let allOrders = [];
+    for (const order of orders) {
+      allOrders = [
+        ...allOrders,
+        await order.populate("user", ["fName", "lName", "email"]),
+      ];
     }
-    try{
-        const item = await Product.findOne({name:name});
-    if(!item){
-        return res.status(404).json({message: "Item not found", success:false})
-    }
-    const accessToken = jwt.sign({_id: adminId, role:"admin"},process.env.SECRET_TOKEN,{expiresIn: "15m"})
-    return res.status(200).json({item, accessToken})
-    }catch(error){
-        
-        console.log(error.message)
-        return res.status(500).json({message:"Internal Server Error", success: false})
-    }
-}
+    const accessToken = jwt.sign(
+      { _id: adminId, role: "admin" },
+      process.env.SECRET_TOKEN,
+      { expiresIn: "15m" },
+    );
+    return res
+      .status(200)
+      .json({ orders: allOrders, totalOrders, accessToken });
+  } catch (error) {
+    console.log(error.message);
+    return res
+      .status(500)
+      .json({ message: "Internal server error", success: false });
+  }
+};
 
+const getOrder = async (req, res) => {
+  const { orderId } = req.params;
+  const adminId = req.userId;
+  if (!orderId) {
+    return res
+      .status(400)
+      .json({ message: "No query defined", success: false });
+  }
+  try {
+    const order = await Order.findById(orderId).populate("user", [
+      "fName",
+      "lName",
+      "email",
+    ]);
+    if (!order) {
+      return res
+        .status(404)
+        .json({ message: "No order found", success: false });
+    }
+    const accessToken = jwt.sign(
+      { _id: adminId, role: "admin" },
+      process.env.SECRET_TOKEN,
+      { expiresIn: "15m" },
+    );
+    return res.status(200).json({ order, accessToken });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Internal server error", success: false });
+  }
+};
 
-const restockProduct = async(req, res)=>{
-    const {name, price, brand, description, stock, color} = req.body
-    const image = req.file?.filename ?? req.body.image
-    const adminId = req.userId
-    if(!name){
-        return res.status(401).json({message:"Product name not provided", success: false})
-    }
-    try{
-        await Product.findOneAndUpdate({name: name}, {name:name, price:price, brand:brand, description:description, stock:stock, color:color, image: '../uploads/shoe-items/' + image})
-        const accessToken = jwt.sign({_id: adminId, role:"admin"},process.env.SECRET_TOKEN,{expiresIn: "15m"})
-        return res.status(200).json({message:"Product Updated", success:true, accessToken})
-    }
-    catch(error){
-        
-        console.log(error.message)
-        return res.status(500).json({message:"Internal server error"})
-    }
-}
-
-const deleteProduct   = async(req, res)=>{
-    const {name} = req.body
-    const adminId = req.userId
-    if(!name){
-        return res.status(401).json({message:"Product name not provided", success: false})
-    }
-    try{
-        const findItem = await Product.findOne({name: name})
-        const accessToken = jwt.sign({_id: adminId, role:"admin"},process.env.SECRET_TOKEN,{expiresIn: "15m"})
-        if(!findItem){
-            return res.status(404).json({message:"Product does not exist", success: false})
-        } 
-        await Product.findOneAndDelete({name:name})
-        return res.status(204).json({message:"Product deleted", success: true, accessToken})
-    }
-    catch(error){
-        
-        console.log(error.message)
-        return res.status(500).json({message:"Internal server error"})        
-    }
-}
-
-
-const getOrders = async(req, res)=>{
-    try{
-        const adminId = req.userId
-        const skip = parseInt(req.query.skip) || 0
-        const totalOrders = await Order.countDocuments({});
-        const orders = await Order.find({status:"Pending"}).skip(skip).limit(5).sort({createdAt: -1})
-        let allOrders = []
-        for(const order of orders){
-            allOrders = [...allOrders, await order.populate('user', ['fName', 'lName', 'email'])]
-        }
-        console.log(allOrders)
-        const accessToken = jwt.sign({_id: adminId, role:"admin"},process.env.SECRET_TOKEN,{expiresIn: "15m"})
-        return res.status(200).json({orders: allOrders, totalOrders, accessToken})
-    }catch(error){
-        console.log(error.message)
-        return res.status(500).json({message: "Internal server error", success: false})
-    }
-}
-
-const getOrder = async(req , res)=>{
-    const { orderId } = req.params
-    const adminId = req.userId
-    if(!orderId){
-        return res.status(400).json({message: "No query defined", success:false})
-    }
-    try{
-        const order = await Order.findById(orderId).populate('user', ['fName', 'lName', 'email']);
-        if(!order){
-            return res.status(404).json({message:"No order found", success:false})
-        }
-         const accessToken = jwt.sign({_id: adminId, role:"admin"},process.env.SECRET_TOKEN,{expiresIn: "15m"})
-        return res.status(200).json({order, accessToken})
-    }catch(error){
-        return res.status(500).json({message: "Internal server error", success: false})
-    }
-}
-
-
-export {login, getAdmin, logout, addProduct, getProduct, restockProduct, deleteProduct, getProducts, getOrders, getOrder, refresh}
+export {
+  login,
+  getAdmin,
+  logout,
+  addProduct,
+  getProduct,
+  restockProduct,
+  deleteProduct,
+  getProducts,
+  getOrders,
+  getOrder,
+  refresh,
+};
--- frontend/src/admin/pages/Orders.tsx
+++ frontend/src/admin/pages/Orders.tsx
@@ -1,96 +1,128 @@
-import {useEffect , useState} from "react"
-import { useParams,useNavigate, useSearchParams } from "react-router-dom"
-import OrderPagination from "../components/OrderPagination"
+import { useEffect, useState } from "react";
+import { useParams, useNavigate, useSearchParams } from "react-router-dom";
+import OrderPagination from "../components/OrderPagination";
 
-import { AxiosError } from "axios"
-import { useAdminTokenStorage }  from "../../store/adminToken"
+import { AxiosError } from "axios";
+import { useAdminTokenStorage } from "../../store/adminToken";
 
-import instance from "../../api/api"
+import instance from "../../api/api";
 
-
-interface Orders{
-  _id: string,
-  user?:{
-  email: string,
-  fName: string,
-  lName: string,
-  },
-  totalPrice: number,
-  address:{
-    city: string,
-    subCity: string,
-    streetName: string,
-    houseNumber: string,
-    phoneNumber: string
-  },
-  status: string
+interface Orders {
+  _id: string;
+  user: {
+    email: string;
+    fName: string;
+    lName: string;
+  };
+  totalPrice: number;
+  address: {
+    city: string;
+    subCity: string;
+    streetName: string;
+    houseNumber: string;
+    phoneNumber: string;
+  };
+  status: string;
 }
 
 const Orders = () => {
-  const {id} = useParams()
-  const [allOrders, setAllOrders] = useState<Orders[]>([])
-  const [ searchParams, setSearchParams ] = useSearchParams()
-  const [orderAmount, setOrderAmount] = useState<number>(0)
-  const navigate = useNavigate()
-  const token = useAdminTokenStorage((state)=>state.token)
-  const saveToken = useAdminTokenStorage((state)=>state.saveToken)
-  const handleOrderFetch = async()=>{
-    try{
-    const getOrders = await instance.get(`/admin/orders?page=${searchParams.get("page")}&skip=${searchParams.get("skip") ?? 0}`, {headers:{
-      Authorization: `bearer ${token}`
-      },withCredentials:true})
-    const {orders, totalOrders, accessToken} = getOrders.data
-    saveToken(accessToken)
-    const newOrders : Orders[] = orders.map((order:Orders)=>{
-      return {_id:order._id, email:order.user.email, fName: order.user.fName, lName: order.user.lName, address:order.address, totalPrice: order.totalPrice, status:order.status}
-    })
+  const { id } = useParams();
+  const [allOrders, setAllOrders] = useState<Orders[]>([]);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [orderAmount, setOrderAmount] = useState<number>(0);
+  const navigate = useNavigate();
+  const token = useAdminTokenStorage((state) => state.token);
+  const saveToken = useAdminTokenStorage((state) => state.saveToken);
 
-    setAllOrders([...newOrders])
+  const handleOrderFetch = async () => {
+    try {
+      const getOrders = await instance.get(
+        `/admin/orders?page=${searchParams.get("page")}&skip=${searchParams.get("skip") ?? 0}`,
+        {
+          headers: {
+            Authorization: `bearer ${token}`,
+          },
+          withCredentials: true,
+        },
+      );
+      const { orders, totalOrders, accessToken } = getOrders.data;
+      console.log(orders);
+      saveToken(accessToken);
 
-    if(totalOrders % 5 === 0){
-        setOrderAmount(totalOrders / 5)
-    } else{
-        setOrderAmount(Math.ceil(totalOrders / 5))
-    }
-    
-    }catch(error){
-      if(error instanceof AxiosError){
-        if(error.status == 401){
-          alert("Unable to access This page, login or sign up")
-          return navigate("/admin")
+      setAllOrders([...orders]);
+
+      if (totalOrders % 5 === 0) {
+        setOrderAmount(totalOrders / 5);
+      } else {
+        setOrderAmount(Math.ceil(totalOrders / 5));
+      }
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        if (error.status == 401) {
+          alert("Unable to access This page, login or sign up");
+          return navigate("/admin");
         }
-        alert(error.response?.data.message)
-      }
-      else if(error instanceof Error){
-          alert(error.message)
+        alert(error.response?.data.message);
+      } else if (error instanceof Error) {
+        alert(error.message);
       }
     }
-  }
-  useEffect(()=>{
-    handleOrderFetch()  
-  },[searchParams])
+  };
+  useEffect(() => {
+    handleOrderFetch();
+  }, [searchParams]);
 
   return (
     <section className="w-full relative top-16 md:static">
-      {allOrders.length === 0 ? (<p className="w-full text-center  md:text-lg font-semibold">No orders found</p>) :(<ul className="w-full pl-4 pt-2 flex justify-start flex-wrap gap-6">
-        {allOrders.map((order)=>{
-          return<li key={order._id} className="w-[100%] md:w-[35%] h-[52vh] shadow-lg shadow-black rounded-md pt-2 pl-2">
-          <p className="text-base md:text-lg">Name: {order.fName} {order.lName}</p>
-          <p className="text-base md:text-lg">Email: {order.email}</p>
-          <p className="text-base md:text-lg">City: {order.address.city}</p>
-          <p className="text-base md:text-lg">Sub-city: {order.address.subCity}</p>
-          <p className="text-base md:text-lg">Street-Name: {order.address.streetName}</p>
-          <p className="text-base md:text-lg">Total: {order.totalPrice} ETB</p>
-          <p className="text-base md:text-lg">Status: {order.status}</p>
-          <section className="w-full h-20 flex justify-center items-center">
-            <button className="cursor-pointer rounded-md bg-slate-950 text-white text-lg font-semibold w-[30%] h-[60%] active:border-inset-2 active:border-white" onClick={()=>navigate(`/dashboard/${id}/order/${order._id}`)}>View Info</button>
-          </section>
-          </li>
-        })}
-      </ul>)} 
-      <OrderPagination orderAmount={orderAmount}/>
+      {allOrders.length === 0 ? (
+        <p className="w-full text-center  md:text-lg font-semibold">
+          No orders found
+        </p>
+      ) : (
+        <ul className="w-full pl-4 pt-2 flex justify-start flex-wrap gap-6">
+          {allOrders.map((order) => {
+            return (
+              <li
+                key={order._id}
+                className="w-screen md:w-[35%] h-[52vh] shadow-lg shadow-black rounded-md pt-2 pl-2"
+              >
+                <p className="text-base md:text-lg">
+                  Name: {order.user.fName} {order.user.lName}
+                </p>
+                <p className="text-base md:text-lg">
+                  Email: {order.user.email}
+                </p>
+                <p className="text-base md:text-lg">
+                  City: {order.address.city}
+                </p>
+                <p className="text-base md:text-lg">
+                  Sub-city: {order.address.subCity}
+                </p>
+                <p className="text-base md:text-lg">
+                  Street-Name: {order.address.streetName}
+                </p>
+                <p className="text-base md:text-lg">
+                  Total: {order.totalPrice} ETB
+                </p>
+                <p className="text-base md:text-lg">Status: {order.status}</p>
+                <section className="w-full h-20 flex justify-center items-center">
+                  <button
+                    className="cursor-pointer rounded-md bg-slate-950 text-white text-lg font-semibold w-[30%] h-[60%] active:border-inset-2 active:border-white"
+                    onClick={() =>
+                      navigate(`/dashboard/${id}/order/${order._id}`)
+                    }
+                  >
+                    View Info
+                  </button>
+                </section>
+              </li>
+            );
+          })}
+        </ul>
+      )}
+      <OrderPagination orderAmount={orderAmount} />
     </section>
-  )
-}
+  );
+};
 
-export default Orders
+export default Orders;
